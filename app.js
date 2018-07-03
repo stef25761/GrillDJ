c40cf2f93ffa8bbad8c75c33a5108e519a5d89a0
Bernd Lorenzen <bernd.lorenzen@stud.fh-flensburg.de>
2018-07-03 16:24:33 +0200

    pictures, css and js files now loading correctly.

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 'use strict';
 
 
-
-const app = require('express')();
+const express=require('express');
+const app = express();
 const server = require('http').createServer(app);
 const path=require('path');
 const websocket=require(path.join(__dirname,path.sep,'websocket')).getWsInstance();
 
 websocket.init(server);
 
-app.use('/',(req,res)=>{
-//html ausliefern
-    res.sendFile(path.join(__dirname,path.sep,  'index.html'));
-});
+app.use('/',express.static(path.join(__dirname,path.sep,'index.html')));
+app.use('/',express.static(path.join(__dirname,path.sep,'static')));
+
 server.listen(3000);
