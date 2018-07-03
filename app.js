135133e5f9662b061a9ec8fe4efdf2ea3d7430b9
Bernd Lorenzen <bernd.lorenzen@stud.fh-flensburg.de>
2018-07-03 17:04:08 +0200

    using sendFile for index.html

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const websocket=require(path.join(__dirname,path.sep,'websocket')).getWsInstance
 
 websocket.init(server);
 app.use('/',(req,res)=>{
-    res.send(path.join(__dirname,path.sep,'static',path.sep,'index.html'));
+    res.sendFile(path.join(__dirname,path.sep,'static',path.sep,'index.html'));
 });
 app.use('/',express.static(path.join(__dirname,path.sep,'static',path.sep,'index.html')));
 app.use('/static',express.static(path.join(__dirname,path.sep,'static')));
