7ca8eb62ede5eb039cdc626993267c9a2f194cfe
Bernd Lorenzen <bernd.lorenzen@stud.fh-flensburg.de>
2018-07-03 16:37:48 +0200

    path of index.htm has changed too. now using the correct one

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const websocket=require(path.join(__dirname,path.sep,'websocket')).getWsInstance
 
 websocket.init(server);
 
-app.use('/',express.static(path.join(__dirname,path.sep,'index.html')));
+app.use('/',express.static(path.join(__dirname,path.sep,'static',path.sep,'index.html')));
 app.use('/static',express.static(path.join(__dirname,path.sep,'static')));
 
 server.listen(3000);
