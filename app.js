f25348cf338b46a8709908d4a97f9d572d1a87f8
Bernd Lorenzen <bernd.lorenzen@stud.fh-flensburg.de>
2018-07-03 16:52:05 +0200

    some test

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const path=require('path');
 const websocket=require(path.join(__dirname,path.sep,'websocket')).getWsInstance();
 
 websocket.init(server);
-
-app.use(express.static(path.join(__dirname,path.sep,'static',path.sep,'index.html')));
+app.use('/',(req,res)=>{
+    res.send(path.join(__dirname,path.sep,'static',path.sep,'index.html'));
+});
+app.use('/',express.static(path.join(__dirname,path.sep,'static',path.sep,'index.html')));
 app.use('/static',express.static(path.join(__dirname,path.sep,'static')));
 
 server.listen(3000);
