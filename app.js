775f5f7d3976740c1f083d7255553c5a07c5f815
Bernd Lorenzen <bernd.lorenzen@stud.fh-flensburg.de>
2018-10-05 21:34:13 +0200

    added root path for certbot challenge

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ const spotify = require(path.join(__dirname, path.sep, 'spotify')).spotify();
 
 
 websocket.init(server);
-
+app.get('/index',(req,res)=>{
+   res.send('root')
+});
 app.use('/index', (req, res) => {
     res.sendFile(path.join(__dirname, path.sep, 'static', path.sep, 'index.html'));
 });
